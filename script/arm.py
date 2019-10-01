e172540fafcae6d5a9b310b578349001c49a62b2
vinicius-r-silva <vinicius.r@usp.br>
2019-09-30 21:08:55 -0300

    improved track detection

--- script/arm.py
+++ script/arm.py
@@ -10,13 +10,33 @@ from math import asin
 #from std_msgs.msg import Int8
 from std_msgs.msg import Int32
 from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32
 from std_msgs.msg import Int32MultiArray
-from rosi_defy.msg import ManipulatorJoints
+from rosi_defy.msg import ManipulatorJoints  
+
+
+#-----------------------CONSTS----------------------#   
+#const received form the ur5_Cam when no fire is detected
+_FIRE_NOT_FOUND = 1000
+
+_FIRE_FOUND_X_VALUE =  400
+_FIRE_FOUND_Y_VALUE = -150
+_FIRE_FOUND_Z_VALUE =  600
+_FIRE_NOT_FOUND_X_VALUE =  400
+_FIRE_NOT_FOUND_Y_VALUE = -150
+_FIRE_NOT_FOUND_Z_VALUE =  850
+
+_MAX_JOINT_ANGLE_DIFF = 0.1
 
 
 #-------------------GLOBAL VARIABLES----------------#    
 #publish joint values to ur5 arm
 arm_publisher = rospy.Publisher('/ur5/jointsPosTargetCommand', ManipulatorJoints, queue_size=10)
+
+get_tilt_y_from_imu = False
+
+#it's true when a fire was detect by the ur5 camera
+fire_found = False
 
 #enable hokuyo
 #hokuyo_publisher = rospy.Publisher('/pra_vale/enable_hokuyo', Int8, queue_size=10)
@@ -37,13 +57,20 @@ l2_pow = l2*l2
 #initial pose
 x = 400
 y = -150
-z = 600
+z = 850
 last_x = x
 last_y = y
 last_z = z
 tilt_x = 0
 tilt_y = 0
 tilt_z = 0
+
+#current desired arm joint angles
+joint_angles = [0,0,0,0,0,0]
+
+#makes the arm ignore all icoming requests to position changes
+#goes to false always when the current arm position is equal to the desired position
+wait_pose_change = False
 
 #return UR5 arm joints angles given the desired position
 def cinematicaInversa():
@@ -58,6 +85,8 @@ def cinematicaInversa():
     global tilt_x
     global tilt_y
     global tilt_z
+
+    global joint_angles
 
     print("x: " + str(x) + "  y: " + str(y) + "  z: " + str(z))
 
@@ -74,7 +103,6 @@ def cinematicaInversa():
 
     #a error is given when the desired position is invalid
     try:
-
         #main calculations
         x_const = x - d0
         x_const_pow = x_const*x_const
@@ -98,30 +126,27 @@ def cinematicaInversa():
         s1 = acos(z_const/l_const)
         s2 = acos(d_const/l_const)
 
-        t1 = pi/2 - (t1_const + s2)
-        t2 = pi - t2_const
-        t3 = -(t3_const + s1)
-
         #joint angles
-        t0 = -pi/2 - acos(probe_lenght/base_dist) + atan2(y_const, x_const)
-        t1 = -t1
-        t2 = -t2
-        t3 = -t3
-        t4 = -t0 - pi
-        t5 = tilt_y
+        joint_angles[0] = -pi/2 - acos(probe_lenght/base_dist) + atan2(y_const, x_const)
+        joint_angles[1] = -pi/2 + (t1_const + s2)
+        joint_angles[2] = -pi + t2_const
+        joint_angles[3] = t3_const + s1
+        joint_angles[4] = -joint_angles[0] - pi
 
         #check if t0 and t4 angles exceeds 2*pi
-        if(t0 > 2*pi):
-            t0 -= 2*pi
-        elif(t0 < -2*pi):
-            t0 += 2*pi
-        # if(t4 > 2*pi):
-        #     t4 -= 2*pi
-        # elif(t4 < -2*pi):
-        #     t4 += 2*pi
+        if(joint_angles[0] > 2*pi):
+            joint_angles[0] -= 2*pi
+        elif(joint_angles[0] < -2*pi):
+            joint_angles[0] += 2*pi
+        # if(joint_angles[4] > 2*pi):
+        #     joint_angles[4] -= 2*pi
+        # elif(joint_angles[4] < -2*pi):
+        #     joint_angles[4] += 2*pi
 
-        t0 += tilt_z
-        t1 += tilt_x
+        #configure tilt
+        joint_angles[0] += tilt_z
+        joint_angles[1] += tilt_x
+        joint_angles[5] = tilt_y
 
         #update last postion
         last_x = x
@@ -129,8 +154,8 @@ def cinematicaInversa():
         last_z = z
 
         #print the joint angles 
-        print(str(t0) + ", " + str(t1) + ", " + str(t2) + ", " + str(t3) + ", " + str(t4) + ", " + str(t5))
-        return [t0, t1, t2, t3, t4, t5]
+        print(joint_angles)
+        return joint_angles
     
     except:
         print("erro: posicao fora do alcance do braco robotico")
@@ -145,6 +170,10 @@ def cinematicaInversa():
 
 #receive an absolute x,y,z position and sets in the simulation
 def arm_pos(data):
+    global wait_pose_change
+    if(wait_pose_change):
+        return
+
     global x
     global y
     global z
@@ -161,18 +190,46 @@ def arm_pos(data):
     arm_publisher.publish(joint_variable = pos)
 
 
+#callback from the ur5 camera when it detects fire
 #receive incremental x,y,z position and sets in the simulation
+#the ur5 camera publishes the x distance between the arm and the fire
 def arm_move(data):
+    global wait_pose_change
+    if(wait_pose_change):
+        return
+        
     global x
     global y
     global z
+    global fire_found
     global arm_publisher
     #print(data) #debug
 
-    x += data.data[0]
-    y += data.data[1]
-    z += data.data[2]
-    
+    if(data.data[0] == _FIRE_NOT_FOUND):
+        if(fire_found):
+            fire_found = False
+            wait_pose_change = True
+
+            x = _FIRE_NOT_FOUND_X_VALUE
+            y = _FIRE_NOT_FOUND_Y_VALUE
+            z = _FIRE_NOT_FOUND_Z_VALUE
+        else:
+            x += data.data[0]
+            y += data.data[1]
+            z += data.data[2]
+    else:
+        if(not fire_found):
+            fire_found = True
+            wait_pose_change = True
+
+            x = _FIRE_FOUND_X_VALUE
+            y = _FIRE_FOUND_Y_VALUE
+            z = _FIRE_FOUND_Z_VALUE
+        else:
+            x += data.data[0]
+            y += data.data[1]
+            z += data.data[2]
+
     #get the joints angles
     pos = cinematicaInversa()
     #publish joints angles
@@ -180,7 +237,11 @@ def arm_move(data):
 
 
 #callback from the IMU sensor, make the ur5 arm follow the track automatically
-def arm_tilt(data):
+def arm_imu(data):
+    global wait_pose_change
+    if(wait_pose_change):
+        return
+        
     global tilt_x
     global tilt_y
     global tilt_z
@@ -197,36 +258,73 @@ def arm_tilt(data):
     t1 = +1.0 - 2.0 * (qx * qx + qy * qy)
     tilt_x = atan2(t0, t1)  #x euler angle
 
-    t2 = +2.0 * (qw * qy - qz * qx)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    tilt_y = asin(t2)  #y euler angle
+    if(get_tilt_y_from_imu):
+        t2 = +2.0 * (qw * qy - qz * qx)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        tilt_y = asin(t2)  #y euler angle
 
     t3 = +2.0 * (qw * qz + qx * qy)
     t4 = +1.0 - 2.0 * (qy * qy + qz * qz)
     tilt_z = atan2(t4, t3) + pi/2  #z euler angle
     
+    #tilt_y += 0.05
+    print((tilt_x, tilt_y, tilt_z))
+
     #get the joints angles
     pos = cinematicaInversa()
     #publish joints angles
     arm_publisher.publish(joint_variable = pos)
 
 
-#callback from the ur5 camera when it detects fire
-#the ur5 camera publishes the x distance between the arm and the fire
-def cam_found_fire(data):
-    x_data = data.data
-    global x
-    x += x_data
+#callback from the ur5 camera, make the ur5 arm follow the track automatically
+def arm_tilt(data):
+    global wait_pose_change
+    if(wait_pose_change):
+        return
+        
+    global tilt_y 
+    global get_tilt_y_from_imu
+    # get_tilt_y_from_imu = True
+    # return
+    print ("wait_pose_change")
+
+    if(data.data == -1):
+        get_tilt_y_from_imu = True
+    else:
+        if(get_tilt_y_from_imu == True):
+            get_tilt_y_from_imu = False
+            tilt_y = 0
+            
+        tilt_y += data.data
+
+
+def arm_current_position(data):
+    global wait_pose_change
+    global joint_angles
+
+    if(not wait_pose_change):
+        return
+        
+    for x in range(len(joint_angles)):
+        if(abs(joint_angles[x] - data.joint_variable[x]) > _MAX_JOINT_ANGLE_DIFF):
+           return
+    
+    wait_pose_change = False
+
+
 
 
 def listener():
     rospy.init_node('arm', anonymous=True)
     
     #rospy.Subscriber('/pra_vale/arm_pos', Int32MultiArray, arm_pos)
+    rospy.Subscriber("/ur5/jointsPositionCurrentState", ManipulatorJoints, arm_current_position)
+
+    rospy.Subscriber('/pra_vale/arm_tilt', Float32, arm_tilt)
     rospy.Subscriber('/pra_vale/arm_move', Int32MultiArray, arm_move)
-    rospy.Subscriber('/pra_vale/cam_found_fire', Int32, cam_found_fire)
-    rospy.Subscriber("/sensor/imu", Imu, arm_tilt)
+    #rospy.Subscriber('/pra_vale/cam_found_fire', Int32, cam_found_fire)
+    rospy.Subscriber("/sensor/imu", Imu, arm_imu)
 
     rospy.spin()
 
--- script/my_5cam.py
+++ script/my_5cam.py
@@ -4,18 +4,123 @@ import cv2
 import time
 import rospy
 import numpy as np
+from math import atan2
 from cv_bridge import CvBridge
+from scipy.optimize import leastsq
 
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 from sensor_msgs.msg import Image
-#from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Int32MultiArray
+
+#-----------------------CONSTS----------------------#   
+#const send to the arm node when no fire is detected
+_FIRE_NOT_FOUND = 1000
+
+  
+#consts used on the detection if thre is a track at the cam
+#define the row use on the detection
+_TRACK_DETECTION_ROW = 150
+#define the max qtd of balck pixels
+_TRACK_DETECTION_MAX_PIXELS = 50
 
 
 #-------------------GLOBAL VARIABLES----------------# 
 #enabled = True
 
 #publish joint values to arm.py
-arm_publisher = rospy.Publisher('/pra_vale/cam_found_fire', Int32, queue_size=10)
+arm_move = rospy.Publisher('/pra_vale/arm_move', Int32MultiArray, queue_size=10)
+arm_tilt = rospy.Publisher('/pra_vale/arm_tilt', Float32, queue_size=10)
+
+#desired z (height) position of the arm
+desired_z = 20
+
+
+#----------------------FUNCTIONS----------------------# 
+
+#given two array of values, find the best line that best fit the arrays
+def leastSquare (x_list, y_list):
+    x_array = np.array(y_list)
+    y_array = np.array(x_list)
+            
+    # here, create lambda functions for Line fit
+    # tpl is a tuple that contains the parameters of the fit
+    func=lambda tpl,x_array : tpl[0]*x_array+tpl[1]
+
+    # ErrorFunc is the diference between the func and the y "experimental" data
+    ErrorFunc=lambda tpl,x_array,y_array: func(tpl,x_array)-y_array
+
+    #tplInitial contains the "first guess" of the parameters 
+    tplInitial1=(1.0,2.0)
+    
+    # leastsq finds the set of parameters in the tuple tpl that minimizes
+    tplFinal1,success=leastsq(ErrorFunc,tplInitial1[:],args=(x_array,y_array))
+    return tplFinal1
+
+
+def there_is_track(frame):
+    row = _TRACK_DETECTION_ROW
+    black_pixels_qtd = 0
+    col = 0
+    while col < frame.shape[1]:
+        if np.all(frame[row, col] == 0):
+            black_pixels_qtd += 1
+            if(black_pixels_qtd > _TRACK_DETECTION_MAX_PIXELS):
+                return False
+
+        col += 1
+    
+    return True
+
+
+#given a frame from the track, get the tilt angle of the camera
+#find the firsts black pixels from the botton
+def get_tilt_angle(frame):
+    #consts
+    qtd = 200
+    rows = frame.shape[0]
+    cols = frame.shape[1]
+    current_col = 0
+    pixels_found = 0
+    step = (int)(cols/qtd)
+    upper_limit = (int)((2*rows)/3)
+    botton_limit = (int)(rows - 1)
+
+    #main loop
+    #check the first black pixels from the botton of the image
+    #then call the leastSquare funtion the find the best line
+    cont = 0
+    x_list = []
+    y_list = []
+    while cont < qtd:
+        x = botton_limit   #start from the botton
+        while(x > upper_limit): #to the upperlimit
+            if np.all(frame[x, current_col] == 0): #check if pixel is black
+                x_list.append(x)                   #if it is, count it
+                y_list.append(current_col)
+                # frame[x, current_col][0] = 0     #paint the pixel if it's necessary
+                # frame[x, current_col][1] = 0
+                # frame[x, current_col][2] = 255
+                pixels_found += 1
+                break
+            x -= 1
+
+        cont += 1
+        current_col += step
+
+    #if wasn't found enough pixels, cancel the search
+    if(pixels_found < (qtd / 10)):
+        return [-1,-1]
+        
+    #otherwise, calculates the leastSquare
+    coef = leastSquare(x_list, y_list)
+
+    #print the line found    
+    cv2.line(frame, (0, (int)(coef[1])), (cols, (int)(coef[1] + coef[0]*cols)), (0,0,255), 1)
+
+    angle = atan2(coef[0]*cols, cols)
+    return [angle, coef[1]]
+
+
 
 #ur5Cam Callback
 #find fire in the given image and calculates the x,y coordinates of the fire
@@ -37,6 +142,7 @@ def ur5_callback(data):
     
     
     #Check size of object
+    error = _FIRE_NOT_FOUND
     if(len(contours) > 0 and cv2.contourArea(max(contours,key=cv2.contourArea))>100):
         #Get biggest object found
         fire = max(contours,key=cv2.contourArea)
@@ -56,18 +162,40 @@ def ur5_callback(data):
 
         kp = 1
         if(error > 100):
-            kp = 6
-        elif(error > 6):
+            kp = 10
+        elif(error > 10):
             kp = 3
         elif(error > 3):
             kp = 2
         else:
             kp = 1
+
+        #multiplies the erro to a constant kp
+        error = error/kp
             
-        #publishes to the arm node
-        print(error)
-        arm_publisher.publish(data = (int)(error/kp))
-            
+
+    #check if there is a track on the camera sight
+    if there_is_track(frame): #if there is a track, get the tilt angle from it
+        #get the tilt angle of the camera
+        angle, b = get_tilt_angle(frame)
+    else:                     #ortherwise, just return -1
+        angle = -1
+        b = -1
+    
+    #publish it to the arm node   
+    arm_tilt.publish(data = angle/2)
+
+    #calculates how much the arm has to move in the z axis
+    #keeps the camera in the same height as the the track rolls
+    z = 0
+    if(b != -1):
+        z = (b - desired_z)/5
+
+    #publishes to the arm node
+    arm_move.publish(data = [error, 0, z])
+
+
+    print(error)
     #cv2.imshow("Threshold",mask)
     cv2.imshow("Camera",frame)
     cv2.waitKey(1)
