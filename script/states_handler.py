729689ebd552719f661f5694022f19be1fb6cc90
GSoaresgama <gabrielsgamma@gmail.com>
2019-10-02 16:35:03 -0300

    states_handle comented

--- script/states_handler.py
+++ script/states_handler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 
 import rospy
 from std_msgs.msg import Int32
@@ -19,6 +19,43 @@ _INITIAL_SETUP     = 8
 state =  (1 << _INITIAL_SETUP) | (1 << _ARM_CHANGING_POSE)
 
 
+
+def print_state():
+    global state
+    string = ""
+
+    #enable velodyne detection path planning
+    if (state & (1 << _ENABLE_VELODYME)):
+        string += ("  _ENABLE_VELODYME")
+
+    #moves the robot arm when the position desired is far away
+    if (state & (1 << _ARM_CHANGING_POSE)):
+        print ("_ARM_CHANGING_POSE")
+
+    #keeps the robot arm aligned to the track
+    if (state & (1 << _FOLLOW_TRACK)):
+        print ("_FOLLOW_TRACK")
+
+    #signalize that fire was found in front of the robot
+    if (state & (1 << _FOUND_FIRE_FRONT)):
+        print ("_FOUND_FIRE_FRONT")
+    
+    #signalize that fire was found in the right of the robot
+    if (state & (1 << _FOUND_FIRE_RIGHT)):
+        print ("_FOUND_FIRE_RIGHT")
+    
+    #when the arm is touching the fire
+    if (state & (1 << _FOUND_FIRE_TOUCH)):
+        print ("_FOUND_FIRE_TOUCH")  
+    
+    #detects the fire easel with hokuyo
+    if (state & (1 << _SETTING_UP_HOKUYO)):
+        print ("_SETTING_UP_HOKUYO")
+
+    print(string)
+
+
+
 #callback function called when a node requires a state change
 def set_state(data):
     global state
@@ -26,26 +63,7 @@ def set_state(data):
 
     #print the state change
     print("\n\n\nstate changed: \n\n")
-    if (state & (1 << _ENABLE_VELODYME)):
-        print ("_ENABLE_VELODYME")
-
-    if (state & (1 << _ARM_CHANGING_POSE)):
-        print ("_ARM_CHANGING_POSE")
-
-    if (state & (1 << _FOLLOW_TRACK)):
-        print ("_FOLLOW_TRACK")
-
-    if (state & (1 << _FOUND_FIRE_FRONT)):
-        print ("_FOUND_FIRE_FRONT")
-
-    if (state & (1 << _FOUND_FIRE_RIGHT)):
-        print ("_FOUND_FIRE_RIGHT")
-
-    if (state & (1 << _FOUND_FIRE_TOUCH)):
-        print ("_FOUND_FIRE_TOUCH")  
-
-    if (state & (1 << _SETTING_UP_HOKUYO)):
-        print ("_SETTING_UP_HOKUYO")
+    print_state()
     print("\n\n\n")
 
 
@@ -56,31 +74,7 @@ if __name__ == '__main__':
     rospy.Subscriber("/pra_vale/set_state", Int32, set_state)
     pub = rospy.Publisher('/pra_vale/estados', Int32, queue_size=1)
 
-    print("state_handler launched")
-    if (state & (1 << _ENABLE_VELODYME)):
-        print ("_ENABLE_VELODYME")
-
-    if (state & (1 << _ARM_CHANGING_POSE)):
-        print ("_ARM_CHANGING_POSE")
-
-    if (state & (1 << _FOLLOW_TRACK)):
-        print ("_FOLLOW_TRACK")
-
-    if (state & (1 << _FOUND_FIRE_FRONT)):
-        print ("_FOUND_FIRE_FRONT")
-
-    if (state & (1 << _FOUND_FIRE_RIGHT)):
-        print ("_FOUND_FIRE_RIGHT")
-
-    if (state & (1 << _FOUND_FIRE_TOUCH)):
-        print ("_FOUND_FIRE_TOUCH")  
-
-    if (state & (1 << _SETTING_UP_HOKUYO)):
-        print ("_SETTING_UP_HOKUYO")
-
-    if (state & (1 << _INITIAL_SETUP)):
-        print ("_SETTING_UP_HOKUYO")
-
+    print_state()
     node_sleep_rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
