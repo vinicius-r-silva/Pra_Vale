5d0460571e3003292a9fefa7dc091b4f9cfa0bef
GSoaresgama <gabrielsgamma@gmail.com>
2019-10-17 07:32:15 -0300

    improved organization

--- src/headers/consts.h
+++ src/headers/consts.h
@@ -21,13 +21,16 @@
 #include <iostream>
 #include <math.h>
 
-//defines
-#define _MIN_HIGHT -10 //altura minima para capturar dados do velodyne
-#define _MAX_DIST 5 //distancia maxima que sera processada
-#define _SCALE 40 //aumenta a resolucao dos dados do velodyne
-#define _ADD_GRAY_SCALE 10 //deixa mais definido quais tem mais pontos em z
-#define _MAX_DIST_NEG -2 //distancia maxima negativa que sera processada
-#define _MIN_GRAY_S 100 //minimo de luminosiade para ser processado
+//DEFINES
+
+//Processamento do velodyne
+#define _MIN_HIGHT        -10  //altura minima para capturar dados do velodyne
+#define _MAX_DIST         5    //distancia maxima que sera processada
+#define _SCALE            40   //aumenta a resolucao dos dados do velodyne
+#define _ADD_GRAY_SCALE   10   //deixa mais definido quais tem mais pontos em z
+#define _MAX_DIST_NEG     -2   //distancia maxima negativa que sera processada
+#define _MIN_GRAY_S       100  //minimo de luminosiade para ser processado
+
 
 //Estados
 #define NOTHING              0
@@ -50,42 +53,53 @@
 #define IN_STAIR             18
 #define CLIMB_STAIR          19
 
-//controle do robo
-#define _V0 3.0 //velocidade do robo
-#define _KP 5.5 //constante para o PID
-#define _KP_OBSTACLE 1.2
-#define _MAX_SPEED 4.2 //velocidade maxima do robo em rad/s
-#define _MIN_DIST_FRONT 2.5 //distancia maxima do obstaculo para virar
-#define _DIST_SEGUE_PAREDE 1.5 //distancia ideal para seguir a parede
-#define _MIN_SAFE_DIST_SPIN 0.75 //distancia minima da frente para girar
-#define _MAX_WHEEL_R_SPEED 1.0 // maxima velocidade de rotação das rodas
-#define _MIN_DIST_ESCADA 1.00 //distancia minima para mandar o robo subir
-#define _MIN_DIST_ALIGNE_FRONT_STAIR 1.1 //distancia minima para comecar a alinhar com a esteira para subir a escada
-#define _MAX_DIST_SIDE_ESCADA 0.85 //distancia maxima do robo para esteira na hora da escada
-#define _MIN_DIST_SIDE_ESCADA 0.65 //distancia minima da esteira para subir a escada
-#define _MAX_ERRO_ESCADA 0.05 //erro no angulo maximo permitido na escada
-#define _HORARIO true //sentido de rotacao do robo
-#define _ANTI_HORARIO false //sentido de rotacao do robo
+
+//controle da velocidade do robo
+#define _V0                 3.0  //velocidade do robo
+#define _KP                 5.5  //constante para o PID
+#define _KP_OBSTACLE        1.2  
+#define _MAX_SPEED          4.2  //velocidade maxima do robo em rad/s
+#define _KP_REC             0.7 
+#define _MAX_WHEEL_R_SPEED  1.0  // maxima velocidade de rotação das rodas
 
 
-#define _NEEDS_SPEED true
 
+//deteccao de objetos
+#define _MIN_AREA       25
+#define _MIN_AREA_REC   10 //area minima para utilizar o metodo de recuDir ou recuEsq
 
-#define _KP_REC 0.7
-#define NICE_DIST_TRACK 0.80
-#define FRONT_WHEELS 0.25
-#define REAR_WHEELS 0.37
-#define PLANE 0.12
 
 //informacoes da struct
-#define _FRONT 0
-#define _LEFT 1
-#define _RIGHT 2
-#define _FRONT_LEFT 3
-#define _FRONT_RIGHT 4
-#define _FRONT_MIDLE 5
+#define _FRONT          0
+#define _LEFT           1
+#define _RIGHT          2
+#define _FRONT_LEFT     3
+#define _FRONT_RIGHT    4
+#define _FRONT_MIDLE    5
 
 
-//processamento da imagem
-#define _MIN_AREA 25
-#define _MIN_AREA_REC 10 //area minima para utilizar o metodo de recuDir ou recuEsq
+//Distancias para controle do robo
+
+//Desviar de objetos
+#define _MIN_DIST_FRONT         2.5   //distancia maxima do obstaculo para virar
+#define _MIN_SAFE_DIST_SPIN     0.75  //distancia minima da frente para girar
+#define NICE_DIST_TRACK         0.80  //Distancia ideal para seguir a esteira
+
+//Alinhar com a escada
+#define _MIN_DIST_ESCADA                1.00  //distancia minima para mandar o robo subir
+#define _MIN_DIST_ALIGNE_FRONT_STAIR    1.1   //distancia minima para comecar a alinhar com a esteira para subir a escada
+#define _MAX_DIST_SIDE_ESCADA           0.85  //distancia maxima do robo para esteira na hora da escada
+#define _MIN_DIST_SIDE_ESCADA           0.65  //distancia minima da esteira para subir a escada
+#define _MAX_ERRO_ESCADA                0.05  //erro no angulo maximo permitido na escada
+
+
+//Parametros do sentido
+#define _HORARIO        true  
+#define _ANTI_HORARIO   false 
+
+
+//Subir a  escada, angulos para cada fase de subida do robo
+#define FRONT_WHEELS    0.25 
+#define REAR_WHEELS     0.37
+#define PLANE           0.12
+
